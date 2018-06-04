3f4b3892f6b0f6f82b00657108492d4c123efaca
iammahesh09 <iammahesh.nlr@gmail.com>
2018-06-04 15:54:29 +0530

    Create Componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductComponent } from './products/product-list/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
