1a690a40faa842468a27889417b2cbab92570b2f
iammahesh09 <iammahesh.nlr@gmail.com>
2018-06-05 20:27:40 +0530

    some chages - not required

--- src/app/AppRouting.ts
+++ src/app/AppRouting.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { UsersComponent } from './users/users.component';
-import { ProductComponent } from './products/product-list/product/product.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 import { PageNotFoundComponent } from './page-not-found';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'About', component: AboutComponent },
     { path: 'Users', component: UsersComponent },
-    { path: 'Products', component: ProductComponent },
+    { path: 'ProductsList', component: ProductListComponent },
     { path: '**', component: PageNotFoundComponent }
 ]
 @NgModule({
@@ -24,7 +24,7 @@ export const AppRoutingCompnent = [
     HomeComponent,
     AboutComponent,
     UsersComponent,
-    ProductComponent,
+    ProductListComponent,
     PageNotFoundComponent
 ]
 
