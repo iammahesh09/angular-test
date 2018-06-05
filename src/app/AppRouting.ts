3cdaadfbeb3eedff52057dd75d76e55142da1f94
iammahesh09 <iammahesh.nlr@gmail.com>
2018-06-05 20:12:08 +0530

    Page Not Found

--- src/app/AppRouting.ts
+++ src/app/AppRouting.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { UsersComponent } from './users/users.component';
 import { ProductComponent } from './products/product-list/product/product.component';
+import { PageNotFoundComponent } from './page-not-found';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'About', component: AboutComponent },
     { path: 'Users', component: UsersComponent },
-    { path: 'Products', component: ProductComponent }
+    { path: 'Products', component: ProductComponent },
+    { path: '**', component: PageNotFoundComponent }
 ]
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
@@ -22,6 +24,7 @@ export const AppRoutingCompnent = [
     HomeComponent,
     AboutComponent,
     UsersComponent,
-    ProductComponent
+    ProductComponent,
+    PageNotFoundComponent
 ]
 
---
+++ src/app/page-not-found.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:'page-not-found',
+    template:`<h2 class="text-danger text-center display-2">Page Not Found</h2>`
+})
+
+export class PageNotFoundComponent{
+    constructor(){}
+}
